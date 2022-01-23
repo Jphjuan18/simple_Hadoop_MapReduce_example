811b1842b3b76e58af94a367cc714ef7c094cecf
Juan Hernandez <jphjuan18@gmail.com>
2022-01-23 03:30:24 +0000

    lowercase and remove stop words

--- mapper.py
+++ mapper.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import sys
 
+stop_words = ['the', 'does', 'and', 'until', 'thus']
+
 # get all lines from stdin
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+    # remove leading and trailing whitespace & lowercase
+    line = line.strip().lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+    	if word not in stop_words:
+            print '%s\t%s' % (word, "1")
